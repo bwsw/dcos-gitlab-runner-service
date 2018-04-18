b2f8cb410f4fbb7ec1fcc92eb6aed9631ee7b82c
Bitworks Software, Ltd <bwsw@users.noreply.github.com>
2018-04-18 10:11:44 +0700

    Update register_and_run.sh

--- register_and_run.sh
+++ register_and_run.sh
@@ -99,6 +99,26 @@ mkdir -p $RUNNER_BUILDS_DIR $RUNNER_CACHE_DIR $RUNNER_WORK_DIR
 echo "==> Printing the environment"
 env
 
+# Check for DOCKER_EXTRA_OPTS. If not present set to empty value
+if [ -z ${DOCKER_EXTRA_OPTS+x} ]; then
+    echo "==> Not using DOCKER_EXTRA_OPTS"
+    DOCKER_EXTRA_OPTS=
+else
+    echo "==> Using DOCKER_EXTRA_OPTS"
+    echo ${DOCKER_EXTRA_OPTS}
+fi
+
+echo "==> Launching the Docker daemon..."
+dind dockerd --host=unix:///var/run/docker.sock $DOCKER_EXTRA_OPTS &
+
+# Wait for the Docker daemon to start
+while(! docker info > /dev/null 2>&1); do
+    echo "==> Waiting for the Docker daemon to come online..."
+    sleep 1
+done
+echo "==> Docker Daemon is up and running!"
+
+
 # Termination function
 _getTerminationSignal() {
     echo "Caught SIGTERM signal! Deleting GitLab Runner!"
